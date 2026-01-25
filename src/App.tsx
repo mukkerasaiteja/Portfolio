fb79efd31f121398062963145834b783ce659f14
mukkerasaiteja <mukkerasaiteja@gmail.com>
2026-01-25 16:47:43 -0600

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Navbar } from '@/components/navbar'
 import { HomePage } from '@/pages/home-page'
 import { GuestbookPage } from '@/pages/guestbook-page'
 import { Footer } from './components/Footer'
+import { Analytics } from '@vercel/analytics/next'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
               <div className="absolute inset-0 bg-[linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)] bg-[size:40px_40px] dark:bg-[linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]" />
               <div className="absolute inset-0 bg-white mask-[radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
             </div>
+            <Analytics />
             <Navbar />
 
             <Routes>
