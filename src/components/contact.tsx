bf098dc6cff412c5c03606372274aa3cbbb7b56f
mukkerasaiteja <mukkerasaiteja@gmail.com>
2026-01-25 16:17:07 -0600

    new

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -17,7 +17,7 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import { RESUME } from '@/data/resume'
+
 import {
   Tooltip,
   TooltipContent,
